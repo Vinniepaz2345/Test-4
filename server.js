98205accd73d6c851468c9790a54c61f0c5644f1
Vinniepaz2345 <iamtrading007@gmail.com>
2025-04-08 09:20:37 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+require('dotenv').config();
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+const pair = require('./pair');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pair.html'));
+});
+
+app.get('/pair', async (req, res) => {
+  const number = req.query.number;
+  if (!number) return res.send({ error: 'Number missing' });
+
+  const id = Math.random().toString(36).slice(2);
+  require('./pair')(number, id, res);
+});
+
+app.listen(PORT, () =>
+  console.log(`Vinnie Pairing Server Running at http://localhost:${PORT}`)
+);
